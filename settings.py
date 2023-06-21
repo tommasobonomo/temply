55e9a8e3530bd25967728befa42212c376c1ca68
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2023-06-21 17:50:28 +0200

    Various changes

--- settings.py
+++ settings.py
@@ -2,10 +2,12 @@ import os
 from dataclasses import dataclass, field
 from enum import Enum
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Dict, List
 
-data_dir = Path("data")
-extension = ".txt"
+import hydra
+
+data_dir = Path("data/geneva")
+extension = ".jsonl"
 
 # Disable tokenizers parallelism -- impacts torch dataloaders
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -13,43 +15,55 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 class SplitType(str, Enum):
     train = "train"
-    dev = "dev"
+    validation = "validation"
     test = "test"
 
 
 @dataclass
 class ModelConfig:
-    """Model config. Has fields that are custom and used to configure the model and its trianing, hopefully they should be self-explanatory."""
+    """
+    Model config.
+    Has fields that are custom and used to configure the model and its trianing,
+    hopefully they should be self-explanatory.
+    """
 
-    transformer_model: str = "distilbert-base-uncased"
-    learning_rate: float = 1e-5
-    batch_size: int = 32
+    transformer_model: str = "bert-base-uncased"
+    learning_rate: float = 5e-5
+    batch_size: int = 64
     use_bias: bool = False
     model_dir: Path = Path("models")
-    model_name: Optional[str] = None
+    model_name: str = ""
+    num_warmup_steps: int = 0
+    num_training_steps: int = 0
+    additional_special_tokens: List[str] = field(default_factory=lambda: [])
+    checkpoint_extension: str = ".ckpt"
 
 
 @dataclass
 class TrainerConfig:
     """
-    Trainer config. Will be deconstructed as arguments to initialise a `pytorch_lightning.Trainer`, so one should take care not to add fields that are not compatible with the `Trainer`'s initalisation arguments.
+    Trainer config. Will be deconstructed as arguments to initialise a `pytorch_lightning.Trainer`,
+    so one should take care not to add fields that are not compatible with the `Trainer`'s initalisation arguments.
     """
 
     accelerator: str = "auto"
     fast_dev_run: bool = False
-    max_epochs: int = 10
+    max_epochs: int = 50
+    log_every_n_steps: int = 5
 
 
 @dataclass
 class Config:
     """
-    Root config class. Holds both `ModelConfig` and `TrainerConfig` as fields. The rest of the fields concern how the script should behave (fit and train, only fit, etc.) and where the data is saved and should be loaded from (`split_paths`).
+    Root config class. Holds both `ModelConfig` and `TrainerConfig` as fields.
+    The rest of the fields concern how the script should behave (fit and train, only fit, etc.)
+    and where the data is saved and should be loaded from (`split_paths`).
     """
 
     split_paths: Dict[SplitType, Path] = field(
         default_factory=lambda: {
             SplitType.train: data_dir / f"{SplitType.train}{extension}",
-            SplitType.dev: data_dir / f"{SplitType.dev}{extension}",
+            SplitType.validation: data_dir / f"{SplitType.validation}{extension}",
             SplitType.test: data_dir / f"{SplitType.test}{extension}",
         }
     )
@@ -61,3 +75,12 @@ class Config:
     num_workers: int = 4
     wandb_dir: Path = Path("wandb")
     enable_wandb: bool = True
+    class2id_path: Path = data_dir / "class2id.json"
+    eval_split: SplitType = SplitType.validation
+    data_pct: float = 1.0
+
+
+config_store = hydra.core.config_store.ConfigStore.instance()
+config_store.store(name="config", node=Config)
+config_store.store(name="model", node=ModelConfig)
+config_store.store(name="trainer", node=TrainerConfig)
--- src/dataset.py
+++ src/dataset.py
@@ -124,3 +124,97 @@ class Seq2seqDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
+
+
+class TokenClassificationDataset(Dataset):
+    def __init__(
+        self,
+        config: Config,
+        split: SplitType,
+        tokenizer: PreTrainedTokenizerFast,
+    ) -> None:
+        self.split = split
+        data_path = config.split_paths[split]
+
+        with open(data_path) as f:
+            self.data = [json.loads(line.strip()) for line in f]
+
+        self.tokenizer_fn = lambda text: self.squeeze_tokenized(
+            tokenizer(
+                text,
+                is_split_into_words=True,
+                padding="longest",
+                return_tensors="pt",
+                return_offsets_mapping=True,
+            )
+        )
+        self.decode_batch = lambda input_ids: tokenizer.batch_decode(
+            input_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+        )
+        with open(config.class2id_path) as f:
+            self.class2id = json.load(f)
+
+    @staticmethod
+    def squeeze_tokenized(raw_tokenization_out: BatchEncoding) -> BatchEncoding:
+        """Simple wrapper function that squeezes the tensors in a given BatchEncoding if their dimension is bigger than 1"""
+        if all(tens.dim() > 1 for tens in raw_tokenization_out.values()):
+            squeezed_tokenization = BatchEncoding(
+                data={
+                    tensor_name: tensor.squeeze()
+                    for tensor_name, tensor in raw_tokenization_out.items()
+                }
+            )
+        else:
+            squeezed_tokenization = raw_tokenization_out
+        return squeezed_tokenization
+
+    def __getitem__(self, index: int) -> dict:
+        raw_sample = self.data[index]
+        return raw_sample
+
+    def collate_fn(self, samples: list[dict]) -> Batch:
+        # Tokenize sentence
+        tokenized_sequence = self.tokenizer_fn(
+            [raw_sample["tokens"] for raw_sample in samples]
+        )
+
+        # Keep batch dimension if len(samples) == 1
+        if len(samples) == 1:
+            for key, value in tokenized_sequence.items():
+                tokenized_sequence[key] = value.unsqueeze(0)
+
+        raw_labels = []
+        for sample, attention_mask, offset_mapping in zip(
+            samples,
+            tokenized_sequence.attention_mask,
+            tokenized_sequence.offset_mapping,
+        ):
+            # Token classification target
+            sample_labels = torch.where(
+                attention_mask.to(torch.bool),
+                self.class2id["O"],
+                -100,
+            )
+
+            # Set original tokens labels with their class
+            og_tokens_index = torch.logical_and(
+                offset_mapping[:, 0] == 0, offset_mapping[:, 1] != 0
+            )
+
+            sample_labels[og_tokens_index] = torch.tensor(
+                [self.class2id[label] for label in sample["labels"]]
+            )
+
+            raw_labels.append(sample_labels)
+
+        labels = torch.stack(raw_labels)
+
+        return {
+            "input_ids": tokenized_sequence.input_ids,
+            "attention_mask": tokenized_sequence.attention_mask,
+            "offset_mapping": tokenized_sequence.offset_mapping,
+            "labels": labels,
+        }
+
+    def __len__(self):
+        return len(self.data)
